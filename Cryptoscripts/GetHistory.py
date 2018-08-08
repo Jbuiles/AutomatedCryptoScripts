35395cc0cc17f5d56c1639490307e6125d299100
Jbuiles <35709632+Jbuiles@users.noreply.github.com>
2018-08-08 12:32:21 -0400

    Create GetHistory.py

---
+++ Cryptoscripts/GetHistory.py
@@ -0,0 +1,38 @@
+import urllib.request
+import json 
+import requests
+import time
+#Get Historical Data ETH
+
+def GetHistoricalEth():
+
+	with urllib.request.urlopen("https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&limit=30") as url:
+		dataEth = json.loads(url.read().decode())
+	js = json.dumps(dataEth)
+	f = open('historyEth.json', 'w')
+	f.write(js)
+	f.close()
+#Get Historical Data Btc
+def GetHistoricalBtc():
+
+	with urllib.request.urlopen("https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=30") as url:
+		dataBtc = json.loads(url.read().decode())
+	js = json.dumps(dataBtc)
+	f = open('historyBtc.json', 'w')
+	f.write(js)
+	f.close()
+#Get Historical Data Ltc
+def GetHistoricalLtc():
+
+	with urllib.request.urlopen("https://min-api.cryptocompare.com/data/histoday?fsym=LTC&tsym=USD&limit=30") as url:
+		dataLtc = json.loads(url.read().decode())
+	js = json.dumps(dataLtc)
+	f = open('historyLtc.json', 'w')
+	f.write(js)
+	f.close()
+
+#while True:
+#	GetHistoricalLtc()
+#	GetHistoricalBtc()
+#	GetHistoricalEth()
+#	time.sleep(50)
